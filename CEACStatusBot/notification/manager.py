8c33212ae2f9107d85badfab7ecb045aeadcaab0
Yanshuo Chen <poseidonchan@icloud.com>
2024-07-17 12:06:20 +0800

    Update manager.py

--- CEACStatusBot/notification/manager.py
+++ CEACStatusBot/notification/manager.py
@@ -17,25 +17,25 @@ class NotificationManager():
     def send(self,) -> None:
         res = query_status(self.__location, self.__number, self.__passport_number, self.__surname, self.__captchaHandle)
 
-        if res['status'] == "Refused":
-            import os,pytz,datetime
-            try:
-                TIMEZONE = os.environ["TIMEZONE"]
-                localTimeZone = pytz.timezone(TIMEZONE)
-                localTime = datetime.datetime.now(localTimeZone)
-            except pytz.exceptions.UnknownTimeZoneError:
-                print("UNKNOWN TIMEZONE Error, use default")
-                localTime = datetime.datetime.now()
-            except KeyError:
-                print("TIMEZONE Error")
-                localTime = datetime.datetime.now()
+        # if res['status'] == "Refused":
+        #     import os,pytz,datetime
+        #     try:
+        #         TIMEZONE = os.environ["TIMEZONE"]
+        #         localTimeZone = pytz.timezone(TIMEZONE)
+        #         localTime = datetime.datetime.now(localTimeZone)
+        #     except pytz.exceptions.UnknownTimeZoneError:
+        #         print("UNKNOWN TIMEZONE Error, use default")
+        #         localTime = datetime.datetime.now()
+        #     except KeyError:
+        #         print("TIMEZONE Error")
+        #         localTime = datetime.datetime.now()
 
-            if localTime.hour < 8 or localTime.hour > 22:
-                print("In Manager, no disturbing time")
-                return
-            if localTime.minute > 30:
-                print("In Manager, no disturbing time")
-                return
+        #     if localTime.hour < 8 or localTime.hour > 22:
+        #         print("In Manager, no disturbing time")
+        #         return
+        #     if localTime.minute > 30:
+        #         print("In Manager, no disturbing time")
+        #         return
 
         for notificationHandle in self.__handleList:
             notificationHandle.send(res)
